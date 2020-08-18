5bcd79bfba9ee4495546a65ced9e9e6644e868cb
mamil <mamilgithub@gmail.com>
2020-08-18 06:20:02 +0300

    add system check

--- std/main.cpp
+++ std/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
 #include <iostream>
 
@@ -36,7 +37,7 @@ HNvIfYoF1BBXfeBXy396PJs6WtdBor9 nihao@mail.com)";
 
 void vector_test()
 {
-    std::cout << "##################################" << std::endl;
+    std::cout << "##################################vector_test" << std::endl;
     vector<int> v1;
     v1.push_back(4);
     v1.push_back(3);
@@ -47,9 +48,61 @@ void vector_test()
     {
         std::cout << i << std::endl;
     }
+
+    auto v2 = vector<int>();
+    for (auto i : v2)
+    {
+        std::cout << i << std::endl;
+    }
+}
+
+void map_test()
+{
+    std::cout << "##################################" << std::endl;
+
+    std::map<int, int> in;
+    in[1]=2;
+    std::map<int,std::map<int, int>> test;
+    test[3]=in;
+    for (auto it : test)
+    {
+        std::cout << it.first << std::endl;;
+        for(auto it2: it.second)
+        {
+            std::cout << it2.first << ":" << it2.second << std::endl;
+        }
+    }
+    test.erase(3);
+
+    auto testit = test.find(3)->second;
+    testit.erase(1);
+
+    for (auto it : test)
+    {
+        std::cout << it.first<< std::endl;;
+        for(auto it2: it.second)
+        {
+            std::cout << it2.first << ":" << it2.second << std::endl;
+        }
+    }
+
+    test.erase(3);
+
+    for (auto it : test)
+    {
+        std::cout << it.first<< std::endl;;
+        for(auto it2: it.second)
+        {
+            std::cout << it2.first << ":" << it2.second << std::endl;
+        }
+    }
+    std::cout << "##################################222" << std::endl;
+
+
 }
 int main() {
    string_test();
    vector_test();
+   map_test();
    return 0;
 }
