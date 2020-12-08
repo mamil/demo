58b4d80e9c224e417e37e696431f998ffe99c4c9
mamil <mamilgithub@gmail.com>
2020-12-08 09:53:38 +0200

    update constructor

--- constructor/main.cpp
+++ constructor/main.cpp
@@ -3,20 +3,14 @@
 
 class Empty {
 public:
-    int n_;
-    Empty() = default;
     Empty (const Empty& a) {
         printf ("拷贝构造\n");
-        this->n_ = a.n_;
     }
-
     Empty& operator= (const Empty& a) {
         printf ("赋值构造\n");
-        this->n_ = a.n_;
     }
-    Empty (int n) {
+    Empty () {
         printf ("address: %x\n", this);
-        n_ = n;
     }
     ~Empty() {
         printf ("delete %x\n", this);
@@ -25,7 +19,7 @@ public:
 
 int main()
 {
-    Empty a(1);
+    Empty a;
     Empty c = a;
 
     printf ("=============\n");
