90d810c6f1f57e8fba53522c0a4318735597b3ee
mamil <mamilgithub@gmail.com>
2020-12-10 10:44:35 +0200

    add leetcode 206 226

---
+++ leetcode/206/main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+struct ListNode {
+    ListNode(){};
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* pre;
+//         for (pre = NULL; head; swap(head, pre))
+//         {
+//             swap(pre, head->next);
+//         }
+//         return pre;
+//     }
+// };
+
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         if(head == NULL || head->next == NULL)
+//         {
+//             return head;
+//         }
+//         ListNode* p1 = head;
+//         ListNode* p2 = head->next;
+//         ListNode* p3 = p2->next;
+
+//         while(p3)
+//         {
+//             p2->next = p1;
+//             p1 = p2;
+//             p2 = p3;
+//             p3 = p3->next;
+//         }
+//         p2->next = p1;
+//         head->next = NULL;
+//         return p2;
+//     }
+// };
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+        ListNode* pre = NULL;
+        while(head)
+        {
+            ListNode* tmp = head->next;
+            head->next = pre;
+            pre = head;
+            head = tmp;
+        }
+        return pre;
+    }
+};
+
+int main()
+{
+    ListNode* node1 = new ListNode();
+    ListNode* node2 = new ListNode();
+    ListNode* node3 = new ListNode();
+
+    node1->val = 1;
+    node1->next = node2;
+    node2->val = 2;
+    node2->next = node3;
+    node3->val = 3;
+    node3->next = NULL;
+
+    Solution s;
+    ListNode* node = s.reverseList(node1);
+
+    std::cout<< node->val << std::endl;
+    while (node->next != NULL)
+    {
+        node = node->next;
+        std::cout<< node->val << std::endl;
+
+    }
+
+    return 0;
+}
---
+++ leetcode/226/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root)
+        {
+            swap(root->left, root->right);
+            invertTree(root->left);
+            invertTree(root->right);
+        }
+        return root;
+    }
+};
+
+int main()
+{
+    TreeNode* node1 = new TreeNode(1);
+    TreeNode* node2 = new TreeNode(2);
+    TreeNode* node3 = new TreeNode(3);
+
+    node1->left = node2;
+    node1->right = node3;
+
+    Solution s;
+    TreeNode* node = s.invertTree(node1);
+
+    return 0;
+}
