5f3df39382d057a0c3b99ef9a9a6a315e7c62ca2
mamil <mamilgithub@gmail.com>
2021-01-07 03:44:50 +0200

    add comment for 120

--- leetcode/120/main.cpp
+++ leetcode/120/main.cpp
@@ -6,15 +6,16 @@
 
 using namespace std;
 
-
+/*逆向计算，计算从倒数第二层开始的每层最小值，然后一层层往上推，到[0][0],就是最小值
+*/
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        for (int i = triangle.size() -2; i >= 0; --i)
+        for (int i = triangle.size() -2; i >= 0; --i) // 从倒数第二层开始往上算
         {
-            for (int j = 0; j < triangle[i].size(); ++j)
+            for (int j = 0; j < triangle[i].size(); ++j) // 从倒数第二层开始的每一行
             {
-                triangle[i][j] += std::min(triangle[i+1][j], triangle[i+1][j+1]);
+                triangle[i][j] += std::min(triangle[i+1][j], triangle[i+1][j+1]); // 倒数第二层每个元素的最小值是本元素+下一层中相邻元素中小的那个
             }
         }
         return triangle[0][0];
