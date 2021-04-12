13114d718cf059888181b74c64789ad68eee34d6
mamil <mamilgithub@gmail.com>
2021-04-12 09:14:04 +0300

    do some modify

--- time/main.cpp
+++ time/main.cpp
@@ -3,7 +3,8 @@
 
 int main()
 {
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now().time_since_epoch()).count() << std::endl;;
+    unsigned long time = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()).count();
+    std::cout << time << std::endl;;
     return 0;
 }
