787dc3ca4e09bb0575ca957cdd7e49e0b4bbc49b
Raymonder jin <rayjingithub@gmail.com>
2021-07-01 04:15:55 -0700

    add slice sort

--- slice/main.go
+++ slice/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+
+import (
+	"fmt"
+	"sort"
+)
+
 
 
 type GatherTrackNodeFeature struct {
@@ -62,10 +67,40 @@ func tryCopy(){
 	fmt.Println("g2:",g2)
 }
 
+
+// 结构体定义
+type test struct {
+	value int
+	str   string
+}
+
+func trySort(){
+	fmt.Println("###### sort")
+	s := make([]test, 5)
+	s[0] = test{value: 2, str: "test2"}
+	s[1] = test{value: 4, str: "test4"}
+	s[2] = test{value: 1, str: "test1"}
+	s[3] = test{value: 5, str: "test5"}
+	s[4] = test{value: 3, str: "test3"}
+	fmt.Println("初始化结果:")
+	fmt.Println(s)
+
+	sort.SliceStable(s, func(i, j int) bool {
+		if s[i].value < s[j].value {
+			return true
+		}
+		return false
+	})
+
+	fmt.Println(s)
+
+}
+
 func main(){
 	fmt.Println("start")
 	tryCopy()
 
 	tryNil()
 	
+	trySort()
 }
